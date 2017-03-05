bf823990e99039105065e392cf0a0ecdb3719174
Yoon <hongys97@gmail.com>
2017-03-05 01:34:53 -0500

    Test code + test txt

--- src/MyVector.java
+++ src/MyVector.java
@@ -45,20 +45,25 @@ public class MyVector<E> extends Vector<E> {
             Student student = new Student(name, address, schoolPhone, suNum, homePhone);
             phoneBook.add(student);
             System.out.println(student);
+
+            // Test code
+            //phoneBook.sortByNames(phoneBook);
+
         }
     }
 
     public MyVector(){
         super();
+
     }
 
     //implementing bubble sort for names
-    public void sortbyNames(Vector<Student> students){
+    public void sortByNames(MyVector<Student> students){
         Student student1;
         Student student2;
         NameComparator compareNames = new NameComparator();
 
-        for (int j = students.size()-1;j>0;j++){
+        for (int j = students.size()-1; j>0; j++){
             boolean swapped = false;
 
             for (int i =1; i<students.size()-1;i++ ){
@@ -76,16 +81,18 @@ public class MyVector<E> extends Vector<E> {
 
     }
 
-    public void sortbySUBox(Vector<Student> students){
+    public void sortBySUBox(MyVector<Student> students){
         Student s1;
         Student s2;
         SUBoxComparator compareSU= new SUBoxComparator();
         for(int i=students.size()-1; i>0;i++){
             boolean swapped = false;
+
         for (int j = 1; j<students.size()-1;j++){
             s1 = students.get(j-1);
             s2 = students.get(j);
             int suBoxResult = compareSU.compare(s1,s2);
+
             if(suBoxResult>0){
                 swap(students,j-1,j);
                 swapped = true;
@@ -96,7 +103,7 @@ public class MyVector<E> extends Vector<E> {
         }
 
     }
-    public static void swap(Vector<Student> student, int i, int j){
+    public static void swap(MyVector<Student> student, int i, int j){
         Student temp = student.get(i);
         student.set(i,student.elementAt(j));
         student.set(j,temp);
